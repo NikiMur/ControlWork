e499ff993119624336abf32d1a47c064d7a97488
NikiMur <zik.9507@bk.ru>
2022-08-03 23:31:47 +0300

    написали программу и записал решение

--- zadacha/Program.cs
+++ zadacha/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введи симовлы символы");
+string[] array = new string[5]; 
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine();
+    array[i] = Console.ReadLine(); 
+}
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine();
+    Console.Write(array[i] + "\t");
+
+}
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Строки с колличеством символов не больше 3");
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        Console.WriteLine(array[i]+ "\t");
+    }
+}
+Console.WriteLine();
+
